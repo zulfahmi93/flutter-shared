f6866b03aae19e33e7637ecafa3049cc8946562c
Zulfahmi Ahmad <zulfahmi93@outlook.com>
2021-06-19 16:51:49 +0800

    [settings] Preparation for publish.

--- settings/lib/src/settings_hive.dart
+++ settings/lib/src/settings_hive.dart
@@ -1,4 +1,4 @@
-import 'package:meta/meta.dart';
+import 'package:flutter/foundation.dart';
 import 'package:hive/hive.dart';
 
 import 'settings.dart';
--- settings/lib/src/settings_shared_preferences.dart
+++ settings/lib/src/settings_shared_preferences.dart
@@ -1,4 +1,4 @@
-import 'package:meta/meta.dart';
+import 'package:flutter/foundation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'settings.dart';
